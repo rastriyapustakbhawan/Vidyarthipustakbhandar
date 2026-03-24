2f00314191256c40ffb3a070bd2a534731532f1f
rastriyapustakbhawan <atulmishra26071999@gmail.com>
2026-03-24 16:13:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('v1').then((cache) => cache.addAll(['/', '/index.html'])));
+  e.waitUntil(
+    caches.open('vidyarthi-v1').then((cache) => cache.addAll(['/', '/index.html']))
+  );
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
